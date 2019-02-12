53f454bc82d157dd11869e7fb8674ca7d5e685e9
Ari Chivukula <ari@chivuku.la>
2019-02-12 10:34:31 -0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -20,6 +20,8 @@ variable "AWS_SECRET_ACCESS_KEY" {}
 
 resource "aws_vpc" "VPC" {
   cidr_block = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags {
     Name = "${var.NAME}"
