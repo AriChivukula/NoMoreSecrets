0d0ee1ffee52fb927fcb63084946396038dfa23e
Ari Chivukula <ari@chivuku.la>
2019-02-12 10:09:34 -0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,6 +3,7 @@ set -e
 sudo pip install awscli
 eval $(aws ecr get-login --no-include-email)
 sudo update-ca-certificates
+cp /etc/ssl/certs/ca-certificates.crt .
 docker build -f docker.file -t nomoresecrets:$TRAVIS_BRANCH .
 docker tag nomoresecrets:$TRAVIS_BRANCH 617580300246.dkr.ecr.us-east-1.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
 docker push 617580300246.dkr.ecr.us-east-1.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
