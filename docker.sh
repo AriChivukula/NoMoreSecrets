d1522f80b0935993c8d68fb27b3f65651146020a
Ari Chivukula <ari@chivuku.la>
2019-02-11 20:54:40 -0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 set -e
 
 docker build -t nomoresecrets:$TRAVIS_BRANCH .
-docker tag nomoresecrets:$TRAVIS_BRANCH 617580300246.dkr.ecr.us-east-1.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
-docker push 617580300246.dkr.ecr.us-east-1.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
+docker tag nomoresecrets:$TRAVIS_BRANCH 617580300246.dkr.$AWS_DEFAULT_REGION.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
+docker push 617580300246.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/nomoresecrets:$TRAVIS_BRANCH
