981b0a877371c76efb13c380cf3f5e811af6b263
Ari Chivukula <ari@chivuku.la>
2019-02-12 08:43:27 -0800

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -3,5 +3,5 @@ set -e
 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
 unzip terraform_0.11.11_linux_amd64.zip
 rm terraform_0.11.11_linux_amd64.zip
-./terraform init -backend-config="bucket=nomoresecrets" -backend-config="key=terraform.tfstate" infra
-./terraform plan infra
+./terraform init -backend-config="bucket=nomoresecrets" -backend-config="key=terraform.tfstate" .
+./terraform plan .
