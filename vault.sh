d8c702911f0e7cfabd6a9a0fa740d98b6b362551
Ari Chivukula <ari@chivuku.la>
2019-02-21 10:47:03 -0800

    Update

--- vault.sh
+++ vault.sh
@@ -1,5 +1,5 @@
 set -e
 
-wget https://releases.hashicorp.com/vault/1.0.2/vault_1.0.2_linux_amd64.zip
-unzip vault_1.0.2_linux_amd64.zip
-rm vault_1.0.2_linux_amd64.zip
+wget https://releases.hashicorp.com/vault/1.0.3/vault_1.0.3_linux_amd64.zip
+unzip vault_1.0.3_linux_amd64.zip
+rm vault_1.0.3_linux_amd64.zip
